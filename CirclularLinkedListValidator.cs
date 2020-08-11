be2128b648fd089cb1682a964cad341d37a7ddea
TheLittleCitizen12 <69463032+TheLittleCitizen12@users.noreply.github.com>
2020-08-11 22:51:16 +0300

    Add files via upload

---
+++ CirclularLinkedListValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class CirclularLinkedListValidator 
+    {
+        public bool IsCircular<T>(LinkedList<T> linkedList)
+        {
+           
+
+            Node slow = head, fast = haed;
+            while(slow != null && fast != null && fast != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if(slow == fast)
+                {
+                    return true;
+                }
+
+            }
+            false;
+        }
+    }
+}
